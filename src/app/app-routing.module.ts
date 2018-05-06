a9605ed7649289ffe9e998dff7e323b734044d8f
abdelrahman.lifestyle@gmail.com <Abdo@work1>
2018-05-06 16:49:23 +0200

    131

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,9 @@
+// import { NgModel } from "@angular/forms";
+
+// @NgModel({
+    
+// })
+
+// export class AppRoutingModule {
+
+// }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,9 @@ const appRoutes: Routes = [
       { path: ":id", component: ServerComponent },
       { path: ":id/edit", component: EditServerComponent }
     ]
-  }
+  },
+  {path: "not-found", component : PageNotFoundComponent},
+  {path : '**' , redirectTo: '/not-found'}
 ];
 
 
