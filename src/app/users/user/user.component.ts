84092396051b43b11a12c88a021937cc5f60f272
abdelrahman.lifestyle@gmail.com <Abdo@work1>
2018-05-06 13:51:50 +0200

    124

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, OnInit , OnDestroy } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-user',
@@ -8,6 +9,7 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class UserComponent implements OnInit {
   user: { id: number, name: string };
+  paramsSubscription:Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -17,5 +19,19 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     }
+
+
+    this.paramsSubscription = this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        });
   }
+  
+
+  ngOnDestroy(){
+    this.paramsSubscription.unsubscribe();
+  }
+
 }
